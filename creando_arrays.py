800c8960f2d6ccd7f251e7fb19c376a185b238ac
Daniel Fernando Puliche Cañas <danielpuliche@unicauca.edu.co>
2024-08-13 19:36:09 -0500

    Update creando_arrays.py

---
+++ creando_arrays.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+lista = np.arange(0,10)
+lista = np.arange(0,20)
+lista = np.arange(0,20,2)
+lista = np.zeros(3) #Make an array with zeros
+lista = np.zeros((10,10)) #Make an 2dim array with zeros
+lista = np.ones((10,5)) #Make an 2dim array with zeros
+lista = np.linspace(0,10,10) # Make an array with the same space
+lista = np.linspace(0,10,100)
+lista = np.eye(4) # Identity array 
+lista = np.random.rand(4)
+lista = np.random.rand(4,5)
+lista = np.random.randint(1,15)
+lista = np.random.randint(1,100,(10,10))
+
+print(lista)
